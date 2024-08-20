719bf63a55fbcd401cc7d39ad6b8c17e99c5dcd7
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2024-08-21 00:05:22 +0100

    initial commit

---
+++ src/Componenets/Data.js
@@ -0,0 +1,223 @@
+import img1 from "../assets/images/scific9.jpeg"
+import img2 from "../assets/images/adventure2.jpeg"
+import img3 from "../assets/images/adventure1.jpeg"
+import img4 from "../assets/images/fantasy1.jpeg"
+import img5 from "../assets/images/fantasy2.jpeg"
+import img6 from "../assets/images/horror8.jpeg"
+import img7 from "../assets/images/adventure3.jpeg"
+import img8 from "../assets/images/adventure4.jpeg"
+import img9 from "../assets/images/fantacy3.jpeg"
+import img10 from "../assets/images/fantasy4.jpeg"
+import img11 from "../assets/images/horror9.jpeg"
+import img12 from "../assets/images/horror3.jpeg"
+import img13 from "../assets/images/horror2.jpeg"
+import img14 from "../assets/images/romance1.jpeg"
+import img15 from "../assets/images/romance2.jpeg"
+
+export const Data = [
+    {
+        "id": 1,
+       "title": "The Gods of Foxcroft" ,
+       "book": img1,
+       "quantity" : 1,
+       "price" : 8,
+       "cat": "science-fiction",
+       "des" : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        "id" : 2,
+       "title": "Hiking Journey" ,
+       "book": img2,
+       "quantity" : 1,
+       "price" : 60,
+       "cat" : "adventure",
+       "des" : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        "id" : 3,
+       "title" : "Nature Travel" ,
+       "book" : img3,
+       "quantity" : 1,
+       "price" : 5,
+       "cat" : "adventure",
+       "des" : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        "id" : 4,
+       "title" : "Deep tides" ,
+       "book": img4,
+       "quantity" : 1,
+       "price" : 62,
+       "cat" : "fantasy",
+       "des" : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        "id": 5,
+       "title": "The tree of Life" ,
+       "book": img5,
+       "quantity" : 1,
+       "price" : 10,
+       "cat" : "fantasy",
+       "des" : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        "id": 6,
+       "title": "The moonlight" ,
+       "book": img6,
+       "quantity" : 1,
+       "price" : 50,
+       "cat" : "horror",
+       "des" : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        "id": 7,
+       "title": "The figure from room 312" ,
+       "book": img7,
+       "quantity" : 1,
+       "price" : 26,
+       "cat" : "horror",
+       "des" : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    }
+    
+]
+
+
+
+export const Datae = [
+    {
+        id: 1,
+       title: "The Gods of Foxcroft" ,
+       book: img1,
+       quantity : 1,
+       price : "8",
+       cat : "science-fiction",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id : 2,
+       title: "Hiking Journey" ,
+       book: img2,
+       quantity : 1,
+       price : "8",
+       cat : "adventure",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id : 3,
+       title : "Nature Travel" ,
+       book : img3,
+       quantity : 1,
+       price : "8",
+       cat : "adventure",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id : 4,
+       title : "Deep tides" ,
+       book: img4,
+       quantity : 1,
+       price : "8",
+       cat : "fantasy",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id: 5,
+       title: "The tree of Life" ,
+       book: img5,
+       quantity : 1,
+       price : "8",
+       cat : "fantasy",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id: 6,
+       title: "The moonlight" ,
+       book: img6,
+       quantity : 1,
+       price : "8",
+       cat : "horror",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id : 7,
+       title : "The figure from room 312" ,
+       book : img7,
+       quantity : 1,
+       price : "8",
+       cat : "horror",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id: 8,
+       title: "The invisible Voyage" ,
+       book: img8,
+       quantity : 1,
+       price : "28",
+       cat : "adventure",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },  
+    {
+        id: 9,
+       title: "Jungle adventure" ,
+       book: img9,
+       quantity : 1,
+       price : "12",
+       cat : "adventure",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },  
+    {
+        id: 10,
+       title: "Dark fantasy" ,
+       book: img10,
+       quantity : 1,
+       price : "4",
+       cat : "fantasy",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id : 11,
+       title : "A court Ravens" ,
+       book : img11,
+       quantity : 1,
+       price : "12",
+       cat : "fantasy",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id : 12,
+       title : "The horror writer" ,
+       book : img12,
+       quantity : 1,
+       price : "16",
+       cat : "horror",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id : 13,
+       title : "Frankenstein" ,
+       book : img13,
+       quantity : 1,
+       price : "28",
+       cat : "horror",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id: 14,
+       title: "My first Love" ,
+       book: img14,
+       quantity : 1,
+       price : "25",
+       cat : "romance",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    },
+    {
+        id: 15,
+       title: "Reasons for falling in love" ,
+       book: img15,
+       quantity : 1,
+       price : "2",
+       cat : "romance",
+       des : "the adventure of agile is an interesting book written by Salisu Musa.the adventure of agile is an interesting book written by Salisu Musa. the adventure of agile is an interesting book written by Salisu Musa."
+    }
+    
+]
